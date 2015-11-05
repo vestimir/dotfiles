596fe66ea0cb54f237bea26a4c2f6b667ba7c9a6
Boryana Manolova <b.manolova@yahoo.com>
2015-11-05 13:22:50 +0200

    Replace double with single quotes
    Why:
    
    * Rubocop raises a mistake
    
    This change addresses the need by:
    
    * Replacing the quotes

--- templates/files/spec/models/account_spec.rb
+++ templates/files/spec/models/account_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe Account, type: :model do
   it { should validate_presence_of :email }
   it { should validate_uniqueness_of :email }
-  it { should allow_value("valid@example.com").for(:email) }
-  it { should_not allow_value("invalid").for(:email) }
+  it { should allow_value('valid@example.com').for(:email) }
+  it { should_not allow_value('invalid').for(:email) }
 end
