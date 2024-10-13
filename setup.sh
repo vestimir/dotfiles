740969fd567ce0e7057bb85909fb06e511efa742
Vestimir Markov <me@vestimir.com>
2024-10-13 21:17:41 +0200

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,7 @@
+brew install coreutils curl git stow gitleaks eza bat fd fzf ripgrep lazygit neovim spaceship
+
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.1
+
+ln -s ~/dotfiles/bin/git-sync /usr/local/bin/git-sync
+ln -s ~/dotfiles/bin/git-delete-merged /usr/local/bin/git-delete-merged
+
